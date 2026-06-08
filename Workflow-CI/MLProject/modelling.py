e1d30642c91f766b5c60fe0a3b547367c577036a
jauhan18 <168714343+jauhan18@users.noreply.github.com>
2026-06-08 21:28:21 +0700

    Update modelling.py

--- Workflow-CI/MLProject/modelling.py
+++ Workflow-CI/MLProject/modelling.py
@@ -5,17 +5,20 @@ import mlflow.sklearn
 import os
 import shutil
 
-# Load Data
+# 1. Load Data secara dinamis menggunakan os.path
 SML_dir = os.path.dirname(os.path.abspath(__file__))
 df = pd.read_csv(os.path.join(SML_dir, 'data_bersih.csv'))
+
+# 2. Pemisahan Fitur dan Target
 X = df.drop(columns=['Survived'])
 y = df['Survived']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Train Model
+# 3. Training Model
 model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=42)
 model.fit(X_train, y_train)
 
-# Simpan model sebagai artefak lokal
+# 4. Simpan model sebagai artefak lokal
 model_dir = "titanic_model_artifact"
 if os.path.exists(model_dir):
     shutil.rmtree(model_dir)
